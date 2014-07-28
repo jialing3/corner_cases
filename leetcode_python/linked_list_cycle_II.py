ec5f543facc954139f643cb3ec79b0fdb4120748
jialing3 <jialing@Jialings-MacBook-Pro.local>
2014-07-27 22:26:56 -0400

    initial push of linked_list_cycle_II.py

---
+++ leetcode_python/linked_list_cycle_II.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param head, a ListNode
+    # @return a list node
+    def detectCycle(self, head):
+        if head is None or head.next is None:
+            return None
+        slow = head.next
+        fast = head.next.next
+        counter = 1
+        while fast:
+            if slow == fast:
+                third = head
+                while third != slow:
+                    third = third.next
+                    slow = slow.next
+                return slow
+            if fast.next is None:
+                return None
+            slow = slow.next
+            fast = fast.next.next
+            counter += 1
+        return None
