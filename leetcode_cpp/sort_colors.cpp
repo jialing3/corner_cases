e506d4994e33e9bb11ea832bc05b6c84d0665ae7
jialing3 <jialing@Jialings-MacBook-Pro.local>
2014-08-09 18:03:23 -0400

    make independent if statements

---
+++ leetcode_cpp/sort_colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+
+        int zero = 0, two = n - 1, i = 0;
+        while (i <= two) {
+            if (A[i] == 2) {
+                std::swap(A[i], A[two]);
+                --two;
+            }
+            if (A[i] == 0) {
+                std::swap(A[i], A[zero]);
+                ++i;
+                ++zero;
+            }
+            if (A[i] == 1) {
+                ++i;
+            }
+        }
+    }
+};
