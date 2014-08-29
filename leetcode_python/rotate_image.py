fcbe7f6460c634ebc2b6861f69ea379e31a5838a
jialing3 <jialing@Jialings-MacBook-Pro.local>
2014-08-29 11:29:04 -0400

    rotation is fun!

---
+++ leetcode_python/rotate_image.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param matrix, a list of lists of integers
+    # @return a list of lists of integers
+    def rotate(self, matrix):
+
+        n_rows = n_cols = len(matrix)
+
+        # flip around the diagonal
+        for i in range(n_rows):
+            for j in range(i + 1, n_cols):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # flip left and right for each row
+        for ind, row in enumerate(matrix):
+            matrix[ind] = row[::-1]
