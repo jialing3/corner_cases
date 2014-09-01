ad22377ce8a25591f48f683c03bd8b86abb843fa
jialing3 <jialing@Jialings-MacBook-Pro.local>
2014-08-31 23:47:17 -0400

    O(N) solution

--- leetcode_python/longest_palindromic_substring.py
+++ leetcode_python/longest_palindromic_substring.py
@@ -1,58 +1,35 @@
+# The symmetry of P[i] around palindrome centers allow us to not recompute some P[i]'s.
+
 class Solution:
     # @return a string
     def longestPalindrome(self, s):
-        def get_common_prefix(a, b):
-            if len(a) > len(b):
-                a, b = b, a
-            count = 0
-            for ind in range(len(a)):
-                if b[ind] == a[ind]:
-                    count = ind + 1
-                else:
-                    break
-            return ''.join(a[:count])
+        def pad(s):
+            s_padded = ['#'] * (len(s) * 2 + 1) # handle even and odd the same way
+            for ind in range(len(s)):
+                s_padded[ind * 2 + 1] = s[ind]
+            return '^' + ''.join(s_padded) + '$'
 
 
-        def reverse_str(s):
-            return ''.join(reversed(s))
+        s = pad(s)
+        center = 0 # center of palindrome
+        right = 0 # right edge of palindrome
+        P = [0] * len(s) # P[i] = the max offset length of palindrome around poisition i, 0 to be the shortest
 
+        for i in range(1, len(s) - 1):
+            i_mirror = 2 * center - i
 
-        if s == '':
-            return s
-        if list(reversed(s)) == list(s):
-            return s
-        if len(set(s)) == 1:
-            return s
+            if right > i:
+                P[i] = min(right - i, P[i_mirror])
+            # otherwise no palindrome to the left, and so left at 0 before expansion below
 
+            while s[i + 1 + P[i]] == s[i - 1 - P[i]]:
+                P[i] += 1
+            # expanding right, taking a total of O(N) steps for the entire loop
 
-        #s_padded = ['#'] * (len(s) * 2 + 1) # handle even and odd the same way
-        #for ind in range(len(s)):
-        #    s_padded[ind * 2 + 1] = s[ind]
+            if i + P[i] > right: # palindrome at i expand beyond right, need to define new center and right edge
+                center = i
+                right = i + P[i]
 
-
-        #max_length = 0
-        #max_string = ''
-        #for center_ind in range(len(s_padded)):
-        #    common_prefix = get_common_prefix(s_padded[(center_ind + 1):], reverse_str(s_padded[:center_ind]))
-        #    if len(common_prefix) > max_length:
-        #        max_length = len(common_prefix)
-        #        max_string = reverse_str(common_prefix) + s_padded[center_ind] + common_prefix
-
-
-        # return ''.join([x for x in max_string if x != '#'])
-
-        max_length = 0
-        max_string = ''
-        for center_ind in range(len(s)):
-            common_prefix_odd = get_common_prefix(s[(center_ind + 1):], reverse_str(s[:center_ind]))
-            common_prefix_even = get_common_prefix(s[(center_ind):], reverse_str(s[:center_ind]))
-            if len(common_prefix_odd) * 2 + 1 > len(common_prefix_even) * 2:
-                common_prefix = reverse_str(common_prefix_odd) + s[center_ind] + common_prefix_odd
-            else:
-                common_prefix = reverse_str(common_prefix_even) + common_prefix_even
-            if len(common_prefix) > max_length:
-                max_length = len(common_prefix)
-                max_string = common_prefix
-
-
-        return max_string
+        max_length, center = max((p, i) for i, p in enumerate(P))
+        max_palindrome = s[center - max_length : center + max_length + 1]
+        return ''.join([x for x in max_palindrome if x != '#'])
