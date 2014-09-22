499c85e13daf6298a090af72bca634930fab796a
jialing3 <jialing@Jialings-MacBook-Pro.local>
2014-09-21 22:23:40 -0400

    There is a bug in leetcode test cases: Input:	2,[set(2,1),set(2,2),get(2),set(1,1),set(4,1),get(2)] Output:	[2,2] Expected:	[2,-1]

--- leetcode_python/lru_cache.py
+++ leetcode_python/lru_cache.py
@@ -4,12 +4,19 @@ class LRUCache:
     def __init__(self, capacity):
         self.store = {} # {key: [value, times_called_by_get]}
         self.capacity = capacity
+        self.key_order = {}
 
 
     # @return an integer
     def get(self, key):
         if key in self.store:
             self.store[key][1] += 1
+            if len(self.key_order) > 0:
+                new_cnt = self.store[key][1]
+                if new_cnt not in self.key_order:
+                    self.key_order[new_cnt] = []
+                self.key_order[new_cnt].append(key)
+                self.key_order[new_cnt - 1].remove(key)
             return self.store[key][0]
         else:
             return -1
@@ -19,9 +26,22 @@ class LRUCache:
     # @param value, an integer
     # @return nothing
     def set(self, key, value):
+        if key in self.store:
+            self.store[key][0] = value
+            return
         if len(self.store) >= self.capacity:
-            # the following operation is too costly, when capacity is reached
-            key_to_discard = min(self.store.items(), key=lambda x: x[1][1])[0] # return the first occurrence if multiple entries exist
+            if len(self.key_order) == 0:
+                for k, v in self.store.items():
+                    if v[1] not in self.key_order:
+                        self.key_store[v[1]] = []
+                    self.key_store[v[1]].append(k)
+            cnt = 0
+            while cnt not in self.key_order or len(self.key_order[cnt]) == 0:
+                cnt += 1
+            key_to_discard = self.key_order[cnt].pop()
             self.store.pop(key_to_discard)
         self.store[key] = [value, 0]
-        return None
+        if 0 not in self.key_order:
+            self.key_order[0] = []
+        self.key_order[0].append(key)
+        return
