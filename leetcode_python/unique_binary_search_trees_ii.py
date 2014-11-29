15dcc44f07613ca9932ac070c1dabf89dc3b72aa
jialing3 <jialing@Jialings-MacBook-Pro.local>
2014-11-28 21:26:07 -0500

    pick root node and recurse on left and right subtrees

---
+++ leetcode_python/unique_binary_search_trees_ii.py
@@ -0,0 +1,35 @@
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def __init__(self):
+        self.memo = {}
+
+    def generate_trees_recursive(self, num_list):
+        if num_list in self.memo:
+            return self.memo[num_list]
+        else:
+            if len(num_list) == 0:
+                return [None]
+            elif len(num_list) == 1:
+                return [TreeNode(num_list[0])]
+            else:
+                output = []
+                for root in num_list:
+                    left_nodes = self.generate_trees_recursive(filter(lambda x: x < root, num_list))
+                    right_nodes = self.generate_trees_recursive(filter(lambda x: x > root, num_list))
+                    for left in left_nodes:
+                        for right in right_nodes:
+                            tmp = TreeNode(root)
+                            tmp.left = left
+                            tmp.right =right
+                            output.append(tmp)
+                return output
+
+    # @return a list of tree node
+    def generateTrees(self, n):
+        return self.generate_trees_recursive(tuple(range(1, n + 1)))
