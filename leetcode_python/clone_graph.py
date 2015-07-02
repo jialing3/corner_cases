616bd3eae171a409ceb5c4cf5e17bf41ffd71ea7
jialing3 <jialing@Jialings-MBP.attlocal.net>
2015-07-01 21:19:39 -0700

    DFS or BFS plus hash map, remember to link self-linkage

--- leetcode_python/clone_graph.py
+++ leetcode_python/clone_graph.py
@@ -1,44 +1,74 @@
-# BFS
+'''
+clone_map = {original_node: cloned_node}
+'''
 
 # Definition for a undirected graph node
-# class UndirectedGraphNode:
-#     def __init__(self, x):
-#         self.label = x
-#         self.neighbors = []
+class UndirectedGraphNode:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
 
 class Solution:
     # @param node, a undirected graph node
     # @return a undirected graph node
     def cloneGraph(self, node):
-        if node is None:
-            return None
-        nodes_to_visit = {node: None}
-        nodes_seen = set()
-        cnt = 0
-        while len(nodes_to_visit) > 0:
-            new_nodes = {}
-            current_nodes_copied = []
-            for node in nodes_to_visit.iterkeys():
-                tmp_node = UndirectedGraphNode(node.label)
-                current_nodes_copied.append(tmp_node)
-                if cnt == 0:
-                    output_node = tmp_node
-                if nodes_to_visit[node]:
-                    nodes_to_visit[node].neighbors.append(tmp_node)
-                for neighbor in node.neighbors:
-                    if neighbor not in nodes_seen:
-                        new_nodes[neighbor] = tmp_node # copied source node
-                        nodes_seen.add(neighbor)
-                    elif neighbor == node:
-                        tmp_node.neighbors.append(tmp_node)
-                cnt += 1
-            # same-level connection
-            for node_ind, node in enumerate(nodes_to_visit.iterkeys()):
-                for neighbor in node.neighbors:
-                    if neighbor in nodes_to_visit:
-                        neighbor_copied = current_nodes_copied[nodes_to_visit.keys().index(neighbor)]
-                        current_nodes_copied[node_ind].neighbors.append(neighbor_copied)
-            nodes_to_visit = new_nodes
-        return output_node
+        clone_map = {} # double as visited set
+        stack = []
+        if node:
+            stack.append(node)
+            clone_map[node] = UndirectedGraphNode(node.label)
+        while stack:
+            current = stack.pop()
+            for neighbor in current.neighbors:
+                if neighbor not in clone_map:
+                    clone_map[neighbor] = UndirectedGraphNode(neighbor.label)
+                    stack.append(neighbor)
+                # important, even if the neighbor is already cloned, still need to check linkage
+                clone_map[current].neighbors.append(clone_map[neighbor])
 
-        
+        return clone_map.get(node, None)
+
+    def BFS(self, node):
+        queue = []
+        visited = set()
+        if node:
+            queue.append(node)
+            visited.add(node)
+        while queue:
+            current = queue.pop(0) # O[n], FIFO
+            print current.label
+            for neighbor in current.neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+        return
+
+    def DFS(self, node):
+        stack = []
+        visited = set()
+        if node:
+            stack.append(node)
+            visited.add(node)
+        while stack:
+            current = stack.pop() # FILO
+            print current.label
+            for neighbor in current.neighbors:
+                if neighbor not in visited:
+                    stack.append(neighbor)
+                    visited.add(neighbor)
+        return
+
+
+
+if __name__ == '__main__':
+    node1 = UndirectedGraphNode(1)
+    node2 = UndirectedGraphNode(2)
+    node3 = UndirectedGraphNode(3)
+    node4 = UndirectedGraphNode(4)
+    node1.neighbors = [node2, node3]
+    node2.neighbors = [node1, node4]
+    node3.neighbors = [node1]
+
+    sol = Solution()
+    sol.BFS(node1) # 1, 2, 3
+    sol.DFS(node1) # 1, 3, 2
