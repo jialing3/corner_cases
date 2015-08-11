6cbf6924917d4ac4e4448f9f33e64eab95951435
jialing3 <jialing@Jialings-MBP.attlocal.net>
2015-08-10 21:49:06 -0700

    edges

--- leetcode_python/contains_duplicate_iii.py
+++ leetcode_python/contains_duplicate_iii.py
@@ -4,19 +4,31 @@ class Solution:
     # @param {integer} t
     # @return {boolean}
     def containsNearbyAlmostDuplicate(self, nums, k, t):
+        if k == 0 or t < 0:
+            return False
+
         if len(nums) < k:
             sliding_window = nums[:]
             sliding_window.sort()
             diff = [a - b for a, b in zip(sliding_window[:-1], sliding_window[1:])]
             return any([-t <= d <= t for d in diff])
 
+
         sliding_window = sorted(nums[:k])
         for ind in range(k, len(nums)):
-            to_insert = self.insert(sliding_window, nums[ind])
-            sliding_window = sliding_window[:to_insert] + [nums[ind]] + sliding_window[to_insert:]
-            diff = [a - b for a, b in zip(sliding_window[:-1], sliding_window[1:])]
-            if any([-t <= d <= t for d in diff]):
+            is_found, position_to_insert = self.insert(sliding_window, nums[ind])
+            if is_found:
                 return True
+            else:
+                if position_to_insert == 0:
+                    if abs(nums[ind] - sliding_window[position_to_insert]) <= t:
+                        return True
+                elif position_to_insert == len(sliding_window):
+                    if abs(nums[ind] - sliding_window[position_to_insert - 1]) <= t:
+                        return True
+                elif abs(nums[ind] - sliding_window[position_to_insert - 1]) <= t or abs(nums[ind] - sliding_window[position_to_insert]) <= t:
+                    return True
+            sliding_window.insert(position_to_insert, nums[ind])
             sliding_window.remove(nums[ind - k])
         else:
             return False
@@ -31,6 +43,5 @@ class Solution:
             elif lst[mid] < target:
                 left = mid + 1
             else:
-                left = mid
-                break
-        return left
+                return (True, mid)
+        return (False, left)
