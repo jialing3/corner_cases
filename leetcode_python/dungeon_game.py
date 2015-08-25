2e8ec8c1fa10f955c00eca7be2de4a7d410b9242
jialing3 <jialing@Jialings-MBP.attlocal.net>
2015-08-24 19:44:29 -0700

    walk backward

--- leetcode_python/dungeon_game.py
+++ leetcode_python/dungeon_game.py
@@ -1,65 +1,25 @@
-class Solution:
-    def prune(self, cell):
-        # fix one variable, maximize the other
-        tmp_dict = {}
-        for running_total, path_min in cell:
-            if running_total not in tmp_dict:
-                tmp_dict[running_total] = path_min
-            else:
-                tmp_dict[running_total] = max(path_min, tmp_dict[running_total])
-        cell = tmp_dict.items()
-        tmp_dict = {}
-        for running_total, path_min in cell:
-            if path_min not in tmp_dict:
-                tmp_dict[path_min] = running_total
-            else:
-                tmp_dict[path_min] = max(running_total, tmp_dict[path_min])
-        cell = [(v, k) for k, v in tmp_dict.items()]
-        return cell
+# walk from bottom right to top left
+# consider if current power level is negative
 
+class Solution:
+    def update_cell(self, last_cell, current_power):
+        return (max(last_cell - current_power, 1)) if current_power > 0 else (last_cell - current_power)
+        # can be combined into max(last_cell - current_power, 1), but code becomes less intuitive
 
     # @param {integer[][]} dungeon
     # @return {integer}
     def calculateMinimumHP(self, dungeon):
         m = len(dungeon)
         n = len(dungeon[0])
-
-        # each cell contains an array of (running_total, path_min) for all possible paths
-        row = [[] for _ in range(n)]
-        # first row
-        row[0].append((dungeon[0][0], dungeon[0][0]))
-        for j in range(1, n):
-            for running_total, path_min in row[j - 1]:
-                running_total += dungeon[0][j]
-                path_min = min(path_min, running_total)
-                row[j].append((running_total, path_min))
-        # other rows
-        for i in range(1, m):
-            # first col
-            cell = []
-            for running_total, path_min in row[0]:
-                running_total += dungeon[i][0]
-                path_min = min(path_min, running_total)
-                cell.append((running_total, path_min))
-            row[0] = cell
-            # other cols
-            for j in range(1, n):
-                cell = []
-                for running_total, path_min in row[j]:
-                    running_total += dungeon[i][j]
-                    path_min = min(path_min, running_total)
-                    cell.append((running_total, path_min))
-                for running_total, path_min in row[j - 1]:
-                    running_total += dungeon[i][j]
-                    path_min = min(path_min, running_total)
-                    cell.append((running_total, path_min))
-                # pick the path that could maximize min(path_min, running_total + constant) in the next step
-                cell = list(set(cell))
-                row[j] = self.prune(cell)
-
-        running_total_s, path_min_s = zip(*row[-1])
-        best_global_min = max(path_min_s)
-        if best_global_min > 0:
-            return 1
-        else:
-            return 1 - best_global_min
+        row = [None for _ in range(n)]
+        row[-1] = self.update_cell(1, dungeon[-1][-1])
+        for j in range(n - 2, -1, -1):
+            row[j] = self.update_cell(row[j + 1], dungeon[-1][j])
+        for i in range(m - 2, -1, -1):
+            row[-1] = self.update_cell(row[-1], dungeon[i][-1])
+            for j in range(n - 2, -1, -1):
+                if row[j] > row[j + 1]:
+                    row[j] = self.update_cell(row[j + 1], dungeon[i][j])
+                else:
+                    row[j] = self.update_cell(row[j], dungeon[i][j])
+        return row[0]
