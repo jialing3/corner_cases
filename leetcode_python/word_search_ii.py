8fa14e8e61b99f3e35164da400cf98c2d92a964a
jialing3 <jialing@Jialings-MBP.attlocal.net>
2015-08-26 17:01:46 -0700

    trie

--- leetcode_python/word_search_ii.py
+++ leetcode_python/word_search_ii.py
@@ -6,32 +6,39 @@ class Solution(object):
         :rtype: List[str]
         """
 
-        words_map = {} # eg. for 'eat', {'e': Fasle, 'ea': False, 'eat': True}
+        words_trie = {}
         for w in words:
-            for i in range(1, len(w)):
-                if w[:i] not in words_map:
-                    words_map[w[:i]] = False
-            words_map[w] = True
+            tmp = words_trie
+            for ind, letter in enumerate(w):
+                if letter not in tmp:
+                    tmp[letter] = {}
+                if ind < len(w) - 1:
+                    tmp = tmp[letter]
+                else: # ind < len(w) - 1
+                    tmp[letter]['__end__'] = w
+
 
         m, n = len(board), len(board[0])
-        output = []
+        output = set()
         for i in range(m):
             for j in range(n):
-                output.extend(self.find_words_starting_from_cell((i, j), board, words_map))
-        return output
+                for w in self.find_words_starting_from_cell((i, j), board, words_trie):
+                    output.add(w)
+        return list(output)
 
-    def find_words_starting_from_cell(self, cell, board, words_map):
-        m, n =len(board), len(board[0])
+    def find_words_starting_from_cell(self, cell, board, words_trie):
+        m, n = len(board), len(board[0])
         row, col = cell
-        nodes_to_visit = [(cell, board[row][col])] if board[row][col] in words_map else [] # cell position and prefix
-        output = []
-        while nodes_to_visit:
-            # DFS
-            current = nodes_to_visit.pop()
-            (row, col), prefix = current
-            for i, j in [[row + 1, col], [row - 1, col], [row, col + 1], [row, col - 1]]:
-                if 0 <= i < m and 0 <= j < n and prefix + board[i][j] in words_map:
-                    nodes_to_visit.append(((i, j), prefix + board[i][j]))
-                    if words_map[prefix + board[i][j]] is True:
-                        output.append(prefix + board[i][j])
+        current_level = [(cell, words_trie[board[row][col]], set())] if board[row][col] in words_trie else []
+        output = set()
+        while current_level:
+            next_level = []
+            for node in current_level:
+                (row, col), tmp, visited_set = node
+                if '__end__' in tmp:
+                    output.add(tmp['__end__'])
+                for i, j in [[row + 1, col], [row - 1, col], [row, col + 1], [row, col - 1]]:
+                    if 0 <= i < m and 0 <= j < n and (i, j) not in visited_set and board[i][j] in tmp:
+                        next_level.append(((i, j), tmp[board[i][j]], visited_set | set([(row, col)])))
+            current_level = next_level[:]
         return output
